5f4c72a7c5accc325b9184c413a8b39ee7da4f71
Peter <peter.nels.hall@gmail.com>
2022-04-14 21:16:52 -0600

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+word = "fuck\nme\n"
+
+character_array = []
+
+for w in word:
+    character_array.append(w)
+
+print(character_array)
+
+character_array_2 = list(word)
+
+print (character_array_2)
